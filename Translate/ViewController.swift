300e3e70a89d1b52ae0b2056806d9fa525196d31
Robert O'Connor <rob.oconnor@gmail.com>
2015-10-16 12:11:15 +0100

    Added Outlets and translation API call to ViewController

--- Translate/ViewController.swift
+++ Translate/ViewController.swift
@@ -9,17 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textToTranslate: UITextView!
+    @IBOutlet weak var translatedText: UITextView!
+    
+    //var data = NSMutableData()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func translate(sender: AnyObject) {
+        
+        let str = textToTranslate.text
+        let escapedStr = str.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+        
+        let langStr = ("en|fr").stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+        
+        let urlStr:String = ("http://api.mymemory.translated.net/get?q="+escapedStr!+"&langpair="+langStr!)
+        
+        let url = NSURL(string: urlStr)
+        
+        let request = NSURLRequest(URL: url!)// Creating Http Request
+        
+        //var data = NSMutableData()var data = NSMutableData()
+        
+        let indicator = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
+        indicator.center = view.center
+        view.addSubview(indicator)
+        indicator.startAnimating()
+        
+        var result = "<Translation Error>"
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { response, data, error in
+            
+            indicator.stopAnimating()
+            
+            if let httpResponse = response as? NSHTTPURLResponse {
+                if(httpResponse.statusCode == 200){
+                    
+                    let jsonDict: NSDictionary!=(try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)) as! NSDictionary
+                    
+                    if(jsonDict.valueForKey("responseStatus") as! NSNumber == 200){
+                        let responseData: NSDictionary = jsonDict.objectForKey("responseData") as! NSDictionary
+                        
+                        result = responseData.objectForKey("translatedText") as! String
+                    }
+                }
+                
+                self.translatedText.text = result
+            }
+        }
+        
+    }
+    
 }
 
