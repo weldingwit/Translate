342dc9b569f953d08ba55cc7a8d0c3c8a36e15d5
Robert O'Connor <rob.oconnor@gmail.com>
2015-10-16 11:59:42 +0100

    Initial Commit

---
+++ Translate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Translate
+//
+//  Created by Robert O'Connor on 16/10/2015.
+//  Copyright © 2015 WIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
