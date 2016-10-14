a85153b7d061c93308e3f5924a7a1b1fe056a600
Robert O'Connor <rob.oconnor@gmail.com>
2016-10-14 10:33:42 +0100

    Updated for Swift 3.0

--- Translate/ViewController.swift
+++ Translate/ViewController.swift
@@ -25,41 +25,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func translate(sender: AnyObject) {
+    @IBAction func translate(_ sender: AnyObject) {
         
         let str = textToTranslate.text
-        let escapedStr = str.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+        let escapedStr = str?.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
         
-        let langStr = ("en|fr").stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+        let langStr = ("en|fr").addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
         
         let urlStr:String = ("http://api.mymemory.translated.net/get?q="+escapedStr!+"&langpair="+langStr!)
         
-        let url = NSURL(string: urlStr)
+        let url = URL(string: urlStr)
         
-        let request = NSURLRequest(URL: url!)// Creating Http Request
+        let request = URLRequest(url: url!)// Creating Http Request
         
         //var data = NSMutableData()var data = NSMutableData()
         
-        let indicator = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
+        let indicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
         indicator.center = view.center
         view.addSubview(indicator)
         indicator.startAnimating()
         
         var result = "<Translation Error>"
         
-        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { response, data, error in
+        NSURLConnection.sendAsynchronousRequest(request, queue: OperationQueue.main) { response, data, error in
             
             indicator.stopAnimating()
             
-            if let httpResponse = response as? NSHTTPURLResponse {
+            if let httpResponse = response as? HTTPURLResponse {
                 if(httpResponse.statusCode == 200){
                     
-                    let jsonDict: NSDictionary!=(try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)) as! NSDictionary
+                    let jsonDict: NSDictionary!=(try! JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)) as! NSDictionary
                     
-                    if(jsonDict.valueForKey("responseStatus") as! NSNumber == 200){
-                        let responseData: NSDictionary = jsonDict.objectForKey("responseData") as! NSDictionary
+                    if(jsonDict.value(forKey: "responseStatus") as! NSNumber == 200){
+                        let responseData: NSDictionary = jsonDict.object(forKey: "responseData") as! NSDictionary
                         
-                        result = responseData.objectForKey("translatedText") as! String
+                        result = responseData.object(forKey: "translatedText") as! String
                     }
                 }
                 
